cd964f54ccca42f56570c4051cab915b945189ed
PawelOchal <opawel262@gmail.com>
2024-03-07 18:09:44 +0100

    created react app

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <p>
+          <a href=""></a>
+        </p>
+      </div>
+    </div>
+  );
+};
